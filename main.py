57176abecdb349d3d8adda5706dc909a992e425f
riccino <riccardorinojosam@gmail.com>
2024-07-12 14:35:22 -0300

    Add 3-second delay at startup, converted video frames to grayscale and applied Gaussian blur, and implemented motion detection using absolute difference and thresholding.

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import time
 import image
 video = cv2.VideoCapture(0)
 
+time.sleep(3)
+first_frame = None
+
 while True:
     check, frame = video.read()
-    cv2.imshow("My video", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+    
+    if first_frame is None:
+        first_frame = gray_frame_gau
+        
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+    thresh_frame = cv2.threshold(delta_frame, 100, 255, cv2.THRESH_BINARY)[1]
+    
+    cv2.imshow("My video", thresh_frame)
     
     key = cv2.waitKey(1)
     if key == ord("q"):
