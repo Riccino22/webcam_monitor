0a91456c61fdc3e958d78ff4d4d60b22bf019207
riccino <riccardorinojosam@gmail.com>
2024-07-24 09:37:16 -0300

    Add status_list to track detection status, modify contour area threshold, and update detect function

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ video = cv2.VideoCapture(0)
 
 time.sleep(3)
 first_frame = None
+status_list = []
 
 def detect():
-    print("Detected")
+    pass
 
 
 while True:
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -27,12 +29,20 @@ while True:
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     for contour in contours:
-        if cv2.contourArea(contour) < 5000:
+        if cv2.contourArea(contour) < 6000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
         if rectangle.any():
+            status = 1
             detect()
+        
+    status_list.append(status)
+    status_list = status_list[-2:]
+    
+    if status_list[0] == 1 and status_list[1] == 0:
+        print(status_list)
+            
     cv2.imshow("video", frame)
     key = cv2.waitKey(1)
     if key == ord("q"):
